f7c34e27cb15d59a6280349159700acb3ae812f1
Islam Temirbek <aitemr16@gmail.com>
2018-06-18 23:15:45 +0600

    feat(Factory): add `Factory` design pattern

---
+++ Factory/Factory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Factory
+//
+//  Created by Islam Temirbek on 6/18/18.
+//  Copyright © 2018 Kolesa LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

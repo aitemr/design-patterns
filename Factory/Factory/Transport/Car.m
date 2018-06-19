b795bb04741c2741e196b0ecf67a8ffd48fee538
Islam Temirbek <aitemr16@gmail.com>
2018-06-19 20:53:42 +0600

    refactor: add files

---
+++ Factory/Factory/Transport/Car.m
@@ -0,0 +1,9 @@
+//
+//  Car.m
+//  Factory
+//
+//  Created by Islam Temirbek on 6/19/18.
+//  Copyright © 2018 Kolesa LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Factory/Factory/Transportation.m
@@ -0,0 +1,9 @@
+//
+//  Transportation.m
+//  Factory
+//
+//  Created by Islam Temirbek on 6/19/18.
+//  Copyright © 2018 Kolesa LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>

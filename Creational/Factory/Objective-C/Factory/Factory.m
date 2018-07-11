2b454cabb381f2e24e26bda68786252e36e363d1
Islam Temirbek <aitemr16@gmail.com>
2018-07-11 20:04:43 +0600

    fix: remove examples

--- Creational/Factory/Objective-C/Factory/Factory.m
+++
@@ -1,9 +0,0 @@
-//
-//  Factory.m
-//  Factory
-//
-//  Created by Islam Temirbek on 6/19/18.
-//  Copyright © 2018 Kolesa LLC. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
--- Creational/Factory/Objective-C/Factory/Transport/Car.m
+++
@@ -1,9 +0,0 @@
-//
-//  Car.m
-//  Factory
-//
-//  Created by Islam Temirbek on 6/19/18.
-//  Copyright © 2018 Kolesa LLC. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>

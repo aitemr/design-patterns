d8c789fc8c0898ccf792d7ee82bd170e366df988
Islam Temirbek <aitemr16@gmail.com>
2018-08-02 00:42:14 +0600

    fix: remove abstract factory

--- DesignPatterns/DesignPatterns/ViewController.swift
+++ DesignPatterns/DesignPatterns/ViewController.swift
@@ -10,21 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var isOriginal: Bool = true
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let factory = getFactory()
-        let client = AbstractFactoryClient(factory: factory)
-        _ = client.buildiPhone()
-        _ = client.buildIPad()
-    }
-    
-    func getFactory() -> DeviceFactory {
-        if isOriginal {
-            return AppleFactory()
-        }
-        return ChinaFactory()
-    }
+    }    
 
 }
